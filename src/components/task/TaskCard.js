9d1a988e1f8f03b5436054fe0a78a4507b814091
unknown <[shrestha_sl@hotmail.com]>
2019-10-09 10:23:50 -0500

    task Component setup

---
+++ src/components/task/TaskCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class TaskCard extends Component {
+    render(){
+        return (
+            <div>
+                <h3>Name:</h3>
+                <p>Complete Date:</p>
+            </div>
+        )
+    }
+}
