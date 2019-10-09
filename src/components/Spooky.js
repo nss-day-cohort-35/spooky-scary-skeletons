d08ab76f6ffbd5a42c12d848754ac3471e159b9a
Alex <o.a.momotyuk@gmail.com>
2019-10-09 11:09:12 -0500

    added login request to database

--- src/components/Spooky.js
+++ src/components/Spooky.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import NavBar from "./nav/NavBar";
 import ApplicationViews from "./ApplicationViews";
 import "./Spooky.css";
+import APIManager from '../modules/APIManager'
 
 class Spooky extends Component {
 
   state = {
-    user: localStorage.getItem('credentials') !== null
+    user: localStorage.getItem('credentials') !== null, //
+
+    query: {
+      table: "users",
+      email: "",
+      password: ""
+    }
+
   };
 
   // Check if credentials are in local storage and returns true/false
@@ -14,10 +22,57 @@ class Spooky extends Component {
 
   setUser = authObj => {
     // For now, just store the email and password that the customer enters into local storage.
+    //
+    //
+    //let credentials = { email: this.state.email, password: this.state.password }
+    //
+
+    console.log("Spooky authObj", authObj)
+
+    console.log("Spooky query before:", this.state.query)
+
+    this.setState(prevState => ({
+      query: {
+        table: prevState.query.table,
+        email: authObj.email,
+        password: authObj.password
+      }
+    }))
+
+    console.log("Spooky query after:", this.state.query)
+
+    APIManager.getRecord(this.state.query)      //
+      //
+      .then(userList => {
+        if (userList.length) {
+          //
+          localStorage.setItem('credentials', JSON.stringify(authObj));
+          this.setState({
+            user: this.isAuthenticated()
+          });
+          /*}
+                    signData = newSignData(
+                      userList[0].userName,
+                      userList[0].password,
+                      userList[0].id
+                    );
+                    sessionStorageData(signData);
+                    document.querySelector("#auth-signin-button").innerHTML = "Sign Out";
+                    removeSignSection("signin-section");
+                    //
+                    document.location.reload(); */
+        } else {
+          alert("Input data is not valid. Try again!");
+        }
+      });
+    //
+    //
+    /*
     localStorage.setItem('credentials', JSON.stringify(authObj));
     this.setState({
       user: this.isAuthenticated()
-    });
+    }); 
+  */
   };
 
   // componentDidMount() {
