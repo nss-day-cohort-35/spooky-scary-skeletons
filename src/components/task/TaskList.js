0323c09e8e6993926fbe90cfac118b7cb379a160
unknown <[shrestha_sl@hotmail.com]>
2019-10-09 10:38:02 -0500

    created list of task

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -1,1 +1,29 @@
 import React, {Component} from 'react';
+
+
+class TaskList extends Component {
+    state = {
+        tasks: []
+    };
+
+    componentDidMount(){
+        APIManager.getAll("tasks").then((task) => {
+            this.setState({
+                tasks: task
+            })
+        })
+    }
+
+
+    render() {
+        return(
+            <div>
+                {this.state.tasks.map(task => (
+                    <TaskCard key = {task.id}
+                              task = {task}
+                    />
+                ))}
+            </div>
+        )
+    }
+}
