092637ffcf4ed37180c07c4c07162d5d822b507e
unknown <[shrestha_sl@hotmail.com]>
2019-10-10 10:16:59 -0500

    userId in task database

--- src/components/task/TaskForm.js
+++ src/components/task/TaskForm.js
@@ -4,13 +4,14 @@ import APIManager from '../../modules/APIManager';
 
 class TaskForm extends Component {
     state = {
-        userId: localStorage.id,
+        userId:"",
         task: "",
         date: "",
         completed: false,
         loadingStaus: false
     }
 
+    
     handleFieldChange = e => {
         const stateToChange = {}
         stateToChange[e.target.id] = e.target.value;
@@ -19,17 +20,19 @@ class TaskForm extends Component {
 
     constructNewTask = e => {
         e.preventDefault();
+        
         if (this.state.taskName === '' || this.state.taskDate === '') {
             window.alert('Field is required')
         } else {
             this.setState({loadingStaus: true})
+            let returnedStorage = localStorage.getItem('credentials')
+            let currentUser = JSON.parse(returnedStorage)[0]
+
             const task = {
-                userId: this.state.userId,
+                userId: currentUser.id,
                 task: this.state.task,
                 date: this.state.date,
                 completed: false,
-
-
             }
             console.log(task)
             APIManager.post(task, "tasks").then(() => this.props.history.push('/tasks'));
