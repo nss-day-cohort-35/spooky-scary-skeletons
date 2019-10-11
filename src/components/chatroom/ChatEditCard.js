12c4bb900b96664a1fc227732bf21168ddd0a683
unknown <mariobud@gmail.com>
2019-10-11 11:43:39 -0500

    Commiting finishing touches

--- src/components/chatroom/ChatEditCard.js
+++ src/components/chatroom/ChatEditCard.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import APIManager from '../../modules/APIManager'
 
 class ChatEditCard extends Component {
+
 
     IsEditAvailable = () => {
         let body;
@@ -9,10 +11,11 @@ class ChatEditCard extends Component {
         if (this.props.message.user.id === this.props.currentUserId) {
             body = <>
                 <div className="card-content">
-                    <p className="card-name">{this.props.message.user.username} :: {this.props.message.date} :</p>
+                    <p className="card-name">{this.props.message.user.username}</p>
                     <p className="card-message">{this.props.message.message}</p>
+                    <p>{this.props.message.date}</p>
                     <button
-                   onClick={() => {this.props.history.push(`/${this.props.message.id}/edit`)}}>Edit
+                   onClick={() => {this.props.history.push(`/chat/${this.props.message.id}/edit`)}}>Edit
                    </button>
                 </div>
             </>
@@ -21,8 +24,9 @@ class ChatEditCard extends Component {
         else {
             body = <>
                 <div className="card-content">
-                    <p className="card-name">{this.props.message.user.username} :: {this.props.message.date} :</p>
+                <p className="card-name" onClick = {()=>this.props.addFriend(this.props.message.user.id)}>{this.props.message.user.username}</p>
                     <p className="card-message">{this.props.message.message}</p>
+                    <p>{this.props.message.date}</p>
                 </div>
             </>
         }
